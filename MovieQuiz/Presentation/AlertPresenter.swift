d5b06257aefb66417e244c44d9cb76312ea42825
Vika Muravyova <muravyova.vi@ya.ru>
2024-06-23 23:57:01 +0300

    Реализованы unit и ui тесты

--- MovieQuiz/Presentation/AlertPresenter.swift
+++ MovieQuiz/Presentation/AlertPresenter.swift
@@ -9,6 +9,7 @@ final class AlertPresenter: PresenterProtocol {
     
     func presentAlert(model: AlertModel) {
         let alert = UIAlertController(title: model.title, message: model.message, preferredStyle: .alert)
+        alert.view.accessibilityIdentifier = "Game results"
         let action = UIAlertAction(title: model.buttonText, style: .default) { _ in
                     model.completion?()
                 }
